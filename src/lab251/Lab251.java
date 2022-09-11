ddd494fcefc096d46bd76c1d8ddfc91f78f6025f
DanaNB1 <dmbaeisa@stu.kau.edu.sa>
2022-09-12 00:58:47 +0300

    Dana commit

--- src/lab251/Lab251.java
+++ src/lab251/Lab251.java
@@ -17,7 +17,7 @@ public class Lab251 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hi, I'm Shatha Almalki :)");
-        System.out.println("Hi, I am Dana Baeisa");
+        System.out.println("Hi, I am Dana Baeisa :)");
         System.out.println("Hi, I am Lama ALghamdi :)");
         System.out.println("Hi, I am Lina Khaled :) ");
         System.out.println("Hi, I am Ragad Sheikh :) ");
