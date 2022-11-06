dc85a1b07194fdd4a19767d921177108c990a9ef
LamaALghamdi22 <lamaalghamdi040@gmail.com>
2022-11-06 19:04:42 +0300

    Lama Hamed Alghamdi - First project commit

---
+++ src/classes/Order.java
@@ -0,0 +1,50 @@
+package classes;
+
+import java.util.ArrayList;
+
+public class Order {
+
+    int QTY;
+    String type;
+      double totalPrice;
+      double price;
+    ArrayList<Order> userOrder = new ArrayList<>();
+
+    public Order(int QTY, String type, double price) {
+        this.QTY = QTY;
+        this.type = type;
+        this.price = price;
+    }
+
+    public Order() {
+    }
+    
+   public Order order(Order newOrder){
+       userOrder.add(newOrder);
+       return newOrder;
+   }
+
+    public int getQTY() {
+        return QTY;
+    }
+
+    public void setQTY(int QTY) {
+        this.QTY = QTY;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    @Override
+    public String toString() {
+        return "Order{" + "QTY=" + QTY + ", type=" + type + ", userOrder=" + userOrder + '}';
+    }
+
+
+
+}
